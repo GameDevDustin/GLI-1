6f5359d67fd4582ee8c00df9a407433c32dc95e2
D Spears <37002982+Xel3@users.noreply.github.com>
2022-06-09 07:56:32 -0500

    Click it or Die challenge completed
    Completed the Click it or Die challenge that changes the color of a cube based on mouse click with Physics.raycast.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = System.Random;
 
 public class PlayerInput : MonoBehaviour
 {
@@ -12,12 +15,18 @@ public class PlayerInput : MonoBehaviour
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             RaycastHit rayHit;
-            Ray rayOrigin = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            Ray rayOrigin = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
             if (Physics.Raycast(rayOrigin, out rayHit))
             {
                 Transform goHit = rayHit.transform;
+                ChangeGOColor(goHit.gameObject);
             }
         }
+    }
+
+    private void ChangeGOColor(GameObject goToBeChanged)
+    {
+        goToBeChanged.GetComponent<MeshRenderer>().material.color = UnityEngine.Random.ColorHSV();
     }
 }
